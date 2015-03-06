7d13364e90beb4c0d3776030863b7b16a0476c4f
Mark D. Thompson <thomesoni@gmail.com>
2015-03-06 08:13:19 -0500

    created incident db and app files

---
+++ sms_map_psql.php
@@ -0,0 +1,26 @@
+<?php
+    // we can get the number of the sender using the 'Fromn' request value
+    $from = $_REQUEST['From'];
+    $msg = $_REQUEST['Body'];
+
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    
+    // break up the message into an array for cleaning
+    $msg_arr = explode(",",$msg);
+    // remove any extra spaces
+    for($i=0; $i<count($msg_arr); $i++){
+        $msg_arr[$i] = trim($msg_arr[$i]);
+    }
+    
+    // insert the incident into the db
+    // 1. get a connection to the database server
+    $dbh=pg_connect("host=database.tcnj.edu dbname=thompsom user=thompsom password=0D1NRULEs");
+    // 2. build our query
+    $sql="INSERT INTO incidents (name,latitude,longitude) VALUES('".$msg_arr[0]."',".$msg_arr[1].",".$msg_arr[2].");";
+    // 3. execute it
+    $result=pg_exec($sql);
+?>
+<Response>
+    <Message>Incident <?php echo $msg_arr[0] . " reported at ". $msg_arr[1] . ", " . $msg_arr[2]. "."; ?></Message>
+</Response>
