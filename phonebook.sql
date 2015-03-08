1e636a2641126b8a80eed2c4b6c128a08050a08b
Mark D. Thompson <thomesoni@gmail.com>
2015-03-08 19:43:18 -0400

    obfuscated numbers

--- phonebook.sql
+++ phonebook.sql
@@ -7,9 +7,9 @@ DROP TABLE IF EXISTS phonebook.address;
 CREATE TABLE phonebook.address(id SERIAL PRIMARY KEY, name TEXT, phone TEXT);
 
 /* insert some records into the phonebook */
-INSERT INTO phonebook.address (name,phone) VALUES('Me','+12152642459');
-INSERT INTO phonebook.address (name,phone) VALUES('Julian','+12678841211');
-INSERT INTO phonebook.address (name,phone) VALUES('Sharleen','+12678646825');
+INSERT INTO phonebook.address (name,phone) VALUES('Me','+12152642000');
+INSERT INTO phonebook.address (name,phone) VALUES('Julian','+12678841000');
+INSERT INTO phonebook.address (name,phone) VALUES('Sharleen','+12678646000');
 
 /* query our database to return all our data to confirm it went in */
 SELECT * FROM phonebook.address;
